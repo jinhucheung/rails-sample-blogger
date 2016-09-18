a70aea80425a2fe94cb5bf7e2991a1895b5b6c51
hikumho <hi.jinhu.zhang@gmail.com>
2016-09-18 17:05:29 +0800

    add:static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # For static_pages
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'static_pages#home'
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @title="Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title","Home | #{@title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title","Help | #{@title}"
+  end
+
+  test "should get about" do 
+    get static_pages_about_url
+    assert_response :success 
+    assert_select "title","About | #{@title}"
+  end 
+
+  test "should get root" do 
+    get root_url
+    assert_response :success
+  end
+
+end
