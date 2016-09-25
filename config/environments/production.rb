e6e5352adbaf92ebcc05e67792cf9e72198d9387
hikumho <hi.jinhu.zhang@gmail.com>
2016-09-25 19:55:13 +0800

    chg env_production for stmp server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
-    :address    => 'smtp.sendgrid.net',
+    :address    => 'smtp.gmail.com',
     :port       => '587',
     :authentication => :plain,
-    :user_name  => ENV['SENDGRID_USERNAME'],
-    :password   => ENV['SENDGRID_PASSWORD'],
+    :user_name  => ENV['GMAIL_USERNAME'],
+    :password   => ENV['GMAIL_PASSWORD'],
     :domain     => 'heroku.com',
     :enable_starttls_auto => true 
   }
