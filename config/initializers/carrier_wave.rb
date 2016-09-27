f85fb5995848ae19e2389fa5703f4c40a9eb1070
hikumho <hi.jinhu.zhang@gmail.com>
2016-09-27 08:09:15 +0800

    chg qiniu bucket domain

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,8 +7,8 @@ if Rails.env.production?
    config.storage             = :qiniu
    config.qiniu_access_key    = ENV['QINIU_ACCESS_KEY']
    config.qiniu_secret_key    = ENV['QINIU_SECRET_KEY']
-   config.qiniu_bucket        = "carrierwave-qiniu-example"
-   config.qiniu_bucket_domain = "carrierwave-qiniu-example.aspxboy.com"
+   config.qiniu_bucket        = "hwblog"
+   config.qiniu_bucket_domain = "oe4x8fz0j.bkt.clouddn.com"
    config.qiniu_bucket_private= true #default is false
    config.qiniu_block_size    = 4*1024*102
    config.qiniu_protocol      = "http"
